68c3de9d6704026e11e1f5cf026b461d8d50a17d
Avi Tevet <fitlogr@avitevet.com>
2013-11-23 22:39:28 -0800

    rails_apps_composer: initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Gardensnap::Application.config.secret_key_base = '66c956b4ec543e2dcbc95da10092f4e72cd23d49bd75aa7a2d7c48acc104c948853c79c7c84331cf869fe3cc47c7e3144ae93f4014b73d5168b56b179af2ebc4'
